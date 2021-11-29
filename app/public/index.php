e4865517eaa94cc23e6c20b9e7e3a55e9ab5b584
Roman Savelyev <roman.savelyev@norbit.ru>
2021-11-29 22:49:54 +0300

    Инициализация

---
+++ app/public/index.php
@@ -0,0 +1,13 @@
+<?php
+echo "<h1>Привет!</h1>";
+echo "<br>";
+echo "Прежде чем начать, посмотри в env() XDEBUG_REMOTE_HOST и заполни";
+echo "<br>";
+echo "Это необходимо для корректной работы xdebug";
+echo "<br>";
+echo "Так же заполни данные о БД";
+echo "<br>";
+echo "<hr>";
+echo "<br>";
+
+phpinfo();
